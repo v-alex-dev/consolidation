9bfde4966ce61e80af4c61e9cf4fcd72c06b3e9c
Alexandre <vensalex1991@gmail.com>
2023-09-20 10:31:34 +0200

    add data

---
+++ javascript/data/gameData.js
@@ -0,0 +1,130 @@
+exports.collection = [
+  {
+    name: "Baldur's Gate III",
+    Developpeur: "Larian Studios",
+    releaseYear: 2023,
+    picture: "../public/games/BG3.webp",
+    genre: ["RPG", "Adventure", "Strategy"],
+    platforms: [
+      "PlayStation 5",
+      "Mac",
+      "Windows",
+    ]
+  },
+  {
+    name: "Total War Warhammer 3",
+    Developpeur: "Creative Assembly",
+    releaseYear: 2022,
+    picture: "../public/games/TotalWar.webp",
+    genre: ["Tacital wargame", "Strategy"],
+    platforms: [
+      "PlayStation 5",
+      "Mac",
+      "Windows",
+    ]
+  },
+  {
+    name: "Final Fantasy XIV",
+    Developpeur: "Square Enix",
+    releaseYear: 2010,
+    picture: "../public/games/FFIV.webp",
+    genre: ["RPG", "MMO", "Adventure"],
+    platforms: [
+      "PlayStation 5",
+      "Playstation 4",
+      "XBOX",
+      "Mac",
+      "Windows",
+    ]
+  },
+  {
+    name: "Sea of Thieves",
+    Developpeur: "Rare",
+    releaseYear: 2018,
+    picture: "../public/games/SOT.webp",
+    genre: ["Action", "MMO", "Action-adventure","Simulation"],
+    platforms: [
+      "XBOX",
+      "Mac",
+      "Windows",
+    ]
+  },
+  {
+    name: "League of Legends",
+    Developpeur: "Riot Games",
+    releaseYear: 2009,
+    picture: "../public/games/LOL.webp",
+    genre: ["Arena", "MMO", "Action-rpg"],
+    platforms: [
+      "Mac",
+      "Windows",
+    ]
+  },
+  {
+    name: "Zelda Breath of the Wild",
+    Developpeur: "Nintendo",
+    releaseYear: 2017,
+    picture: "../public/games/Zelda.webp",
+    genre: ["RPG", "Adventure", "Action"],
+    platforms: [
+      "Nintendo Switch",
+      "Wii U"
+    ]
+  },
+  {
+    name: "Stellaris",
+    Developpeur: "Paradox Development Studio",
+    releaseYear: 2016,
+    picture: "../public/games/Stellaris.webp",
+    genre: ["4X", "Simulation", "Strategy"],
+    platforms: [
+      "Playstation 4",
+      "Linux",
+      "XBOX",
+      "Mac",
+      "Windows",
+    ]
+  },
+  {
+    name: "Mario Kart 8",
+    Developpeur: "Nintendo",
+    releaseYear: 2014,
+    picture: "../public/games/MarioKart.webp",
+    genre: ["Course"],
+    platforms: [
+      "Nintendo switch",
+      "Wii U"
+    ]
+  },
+  {
+    name: "ALTF4",
+    Developpeur: "PIMPKIM",
+    releaseYear: 2021,
+    picture: "../public/games/AltF4.webp",
+    genre: ["Platform", "Indepadant", "Adventure"],
+    platforms: [
+      "PlayStation 5",
+      "Playstation 4",
+      "XBOX",
+      "Mac",
+      "Windows",
+    ]
+  },
+  {
+    name: "Pokemon jaune",
+    Developpeur: "Nintendo",
+    releaseYear: 1998,
+    picture: "../public/games/Pokemon.webp",
+    genre: ["RPG", "Adventure"],
+    platforms: [
+      "Game Boy",
+      "Nintendo 3DS"
+    ]
+  },
+  
+  
+  
+  
+  
+
+]
