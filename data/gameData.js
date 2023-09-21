955f439ed66041aec10f0db65c91cdeaa2ad60e8
v-alex-dev <vensalex1991@gmail.com>
2023-09-21 12:32:55 +0200

    fix bug

--- data/gameData.js
+++ data/gameData.js
@@ -3,7 +3,7 @@ const collection = [
     name: "Baldur's Gate III",
     Developpeur: "Larian Studios",
     releaseYear: 2023,
-    picture: "../../public/games/BG3.webp",
+    picture: "./public/games/BG3.webp",
     genre: ["RPG", "Adventure", "Strategy"],
     platforms: [
       "PlayStation 5",
@@ -15,7 +15,7 @@ const collection = [
     name: "Total War Warhammer 3",
     Developpeur: "Creative Assembly",
     releaseYear: 2022,
-    picture: "../../public/games/TotalWar.webp",
+    picture: "./public/games/TotalWar.webp",
     genre: ["Tactical wargame", "Strategy"],
     platforms: [
       "PlayStation 5",
@@ -27,7 +27,7 @@ const collection = [
     name: "Final Fantasy XIV",
     Developpeur: "Square Enix",
     releaseYear: 2010,
-    picture: "../../public/games/FFIV.webp",
+    picture: "./public/games/FFIV.webp",
     genre: ["RPG", "MMO", "Adventure"],
     platforms: [
       "PlayStation 5",
@@ -41,7 +41,7 @@ const collection = [
     name: "Sea of Thieves",
     Developpeur: "Rare",
     releaseYear: 2018,
-    picture: "../../public/games/SOT.webp",
+    picture: "./public/games/SOT.webp",
     genre: ["Action", "MMO", "Action-adventure","Simulation"],
     platforms: [
       "XBOX",
@@ -53,7 +53,7 @@ const collection = [
     name: "League of Legends",
     Developpeur: "Riot Games",
     releaseYear: 2009,
-    picture: "../../public/games/LOL.webp",
+    picture: "./public/games/LOL.webp",
     genre: ["Arena", "MMO", "Action-rpg"],
     platforms: [
       "Mac",
@@ -64,7 +64,7 @@ const collection = [
     name: "Zelda Breath of the Wild",
     Developpeur: "Nintendo",
     releaseYear: 2017,
-    picture: "../../public/games/Zelda.webp",
+    picture: "./public/games/Zelda.webp",
     genre: ["RPG", "Adventure", "Action"],
     platforms: [
       "Nintendo Switch",
@@ -75,7 +75,7 @@ const collection = [
     name: "Stellaris",
     Developpeur: "Paradox Development Studio",
     releaseYear: 2016,
-    picture: "../../public/games/Stellaris.webp",
+    picture: "./public/games/Stellaris.webp",
     genre: ["4X", "Simulation", "Strategy"],
     platforms: [
       "Playstation 4",
@@ -89,7 +89,7 @@ const collection = [
     name: "Mario Kart 8",
     Developpeur: "Nintendo",
     releaseYear: 2014,
-    picture: "../../public/games/MarioKart.webp",
+    picture: "./public/games/MarioKart.webp",
     genre: ["Course"],
     platforms: [
       "Nintendo switch",
@@ -100,7 +100,7 @@ const collection = [
     name: "ALTF4",
     Developpeur: "PIMPKIM",
     releaseYear: 2021,
-    picture: "../../public/games/AltF4.webp",
+    picture: "./public/games/AltF4.webp",
     genre: ["Platform", "Independent", "Adventure"],
     platforms: [
       "PlayStation 5",
@@ -114,7 +114,7 @@ const collection = [
     name: "Pokemon jaune",
     Developpeur: "Nintendo",
     releaseYear: 1998,
-    picture: "../../public/games/Pokemon.webp",
+    picture: "./public/games/Pokemon.webp",
     genre: ["RPG", "Adventure"],
     platforms: [
       "Game Boy",
